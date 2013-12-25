a3a191bc201912599df23ffd8ebbe91cd8015406
Lucups <lucups@gmail.com>
2013-12-26 00:07:01 +0800

    add entities

---
+++ codes/src/Caiwen/CoreBundle/Entity/Docs.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucups
+ * Date: 13-12-26
+ * Time: 上午12:06
+ */
+
+namespace Caiwen\CoreBundle\Entity;
+
+
+class Docs {
+
+} 
---
+++ codes/src/Caiwen/CoreBundle/Entity/Photo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucups
+ * Date: 13-12-26
+ * Time: 上午12:05
+ */
+
+namespace Caiwen\CoreBundle\Entity;
+
+
+class Photo {
+
+} 
