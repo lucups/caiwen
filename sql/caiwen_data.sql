958d1f8d360ecfcb0587ff263fea1721d51a0393
Lucups <lucups@gmail.com>
2014-01-05 16:17:37 +0800

    update sql

--- sql/caiwen_data.sql
+++ sql/caiwen_data.sql
@@ -1,3 +1,14 @@
-﻿INSERT INTO `album` (`album_id`, `title`) VALUES ('1', '白菜病害');
+﻿# add admin user
+INSERT INTO `user` (`username`, `password`, `email`, `role`)
+  VALUES ('admin', '9281afa7e293ed50310dc6b1639352edc6289631', 'admin@caiwen.org', 0);
+
+# add user
+INSERT INTO `user` (`username`, `password`, `email`, `role`)
+  VALUES ('wnp', '9281afa7e293ed50310dc6b1639352edc6289631', 'wnp@caiwen.org', 1);
+INSERT INTO `user` (`username`, `password`, `email`, `role`)
+  VALUES ('ws', '9281afa7e293ed50310dc6b1639352edc6289631', 'ws@caiwen.org', 1);
+
+# add album
+INSERT INTO `album` (`album_id`, `title`) VALUES ('1', '白菜病害');
 INSERT INTO `album` (`album_id`, `title`) VALUES ('2', '白菜虫害');
 INSERT INTO `album` (`album_id`, `title`) VALUES ('3', '杂草识别');
